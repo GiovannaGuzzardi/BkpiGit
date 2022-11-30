073b2dd30301bb408a8cb0cbe3c453a4402a4f5b
giovannaguzzardi <giovannaguzz13@gmail.com>
2022-11-30 14:40:25 -0300

    "pronto"

--- fast_00/first.01/main.py
+++ fast_00/first.01/main.py
@@ -3,12 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/msg")
-async def mensagem():  # raiz pode ser qualquer nome
+@app.get('/')
+async def raiz():  # raiz pode ser qualquer nome
     return {"message": "Hello World"}
 
 if __name__ == '__main__':
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000,
+# 0.0.0.1 para qualquer pessoas na rede acessar
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
                 log_level="info", reload=True)
